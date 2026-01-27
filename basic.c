1d2953911410e104b6598af66ae8e46d9d01a88c
Siddu-trozan <chsiddeshwar9@gmail.com>
2026-01-27 15:21:17 +0530

    Create basic.c

---
+++ basic.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+
+int main()
+{
+    float P,T,R,CI, Amount;
+    printf("enter the principal amount:");
+    scanf("%f",&P);
+    printf("enter time (in years):");
+    scanf("%f",&T);
+    printf("enter rate of interest:");
+    scanf("%f",&R);
+    Amount=P*pow((1+R/100),T);
+    CI= Amount - P;
+    printf("THE COMPOUND INTEREST IS=%1.2f",CI);;;;;;
+    }
